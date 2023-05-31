a7de292ff8f36709ffd23bcec533ef3f3ae26649
Martin <martin.jourjon42@gmail.com>
2023-05-31 11:07:50 +0200

    maj controller

--- src/main/java/pochemon/store/controller/StoreController.java
+++ src/main/java/pochemon/store/controller/StoreController.java
@@ -1,5 +1,46 @@
 package pochemon.store.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pochemon.dto.StoreOrderDTO;
+import pochemon.dto.StoreTransactionDTO;
+import pochemon.store.mapper.StoreOrderMapper;
+import pochemon.store.mapper.StoreTransactionMapper;
+import pochemon.store.service.StoreService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/stores")
+@CrossOrigin
 public class StoreController {
 
+    @Autowired
+    StoreService storeService;
+
+    @Autowired
+    StoreOrderMapper storeOrderMapper;
+
+    @Autowired
+    StoreTransactionMapper storeTransactionMapper;
+
+    @PostMapping("/sell")
+    public Boolean sellCard(@RequestBody StoreOrderDTO storeOrderDTO) {
+        return storeService.sellCard(storeOrderMapper.toStoreOrder(storeOrderDTO));
+    }
+
+    @PostMapping("/buy")
+    public boolean buyCard(@RequestBody StoreOrderDTO storeOrderDTO) {
+        return storeService.buyCard(storeOrderMapper.toStoreOrder(storeOrderDTO));
+    }
+
+    @GetMapping("/transactions")
+    public List<StoreTransactionDTO> getAllTransactions() {
+        return storeTransactionMapper.toListStoreTransactionDTO(storeService.getAllStoreTransactions());
+    }
+
+    @GetMapping("/orders")
+    public List<StoreOrderDTO> getAllStoreOrders() {
+        return storeOrderMapper.toListStoreOrderDTO(storeService.getAllStoreOrders());
+    }
 }
--- src/main/java/pochemon/store/service/StoreService.java
+++ src/main/java/pochemon/store/service/StoreService.java
@@ -8,6 +8,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import pochemon.dto.CardDTO;
+import pochemon.dto.UserDTO;
+import pochemon.service.CardWebService;
+import pochemon.service.UserWebService;
 import pochemon.store.repository.StoreTransactionRepository;
 import pochemon.store.repository.StoreOrderRepository;
 
@@ -19,15 +23,25 @@ import pochemon.enums.Action;
 public class StoreService {
 
 	@Autowired
+	UserWebService userWebService;
+
+	@Autowired
+	CardWebService cardWebService;
+
+	@Autowired
 	StoreTransactionRepository storeTransactionRepository;
 	
 	@Autowired
 	StoreOrderRepository storeOrderRepository;
 	
-	public List<StoreTransaction> getAllStoreTransaction() {
+	public List<StoreTransaction> getAllStoreTransactions() {
 		return storeTransactionRepository.findAll();
 	}
-	
+
+	public List<StoreOrder> getAllStoreOrders() {
+		return storeOrderRepository.findAll();
+	}
+
 	public Boolean sellCard(StoreOrder storeOrder) {
 		if(storeOrder.getPrice() < 0f) {
 			return false;
@@ -37,28 +51,25 @@ public class StoreService {
 	}
 	
 	public Boolean buyCard(StoreOrder storeOrder) {
-		//TO DO REPRENDRE AVEC RPC ET LA LIB
-		
-		/*
 		// Récupération de la carte à acheter
-		StoreOrder storeOrderToBuy = storeOrderRepository.findByCardId(storeOrder.getCard().getId());
+		StoreOrder storeOrderToBuy = storeOrderRepository.findByCardId(storeOrder.getCardId());
 
 		// Utilisateur qui vend et achète la carte
-		User sellerUser = storeOrderToBuy.getUser();
-		User buyerUser = storeOrder.getUser();
+		UserDTO sellerUser = userWebService.getUser(storeOrderToBuy.getUserId());
+		UserDTO buyerUser = userWebService.getUser(storeOrder.getUserId());
 
 		// Si l'utilisateur a assez d'argent pour acheter la carte
-		if(userService.changeMoney(buyerUser, storeOrderToBuy.getPrice() * (-1))) {
+		if(userWebService.changeMoney(buyerUser, storeOrderToBuy.getPrice() * (-1))) {
 
-			Card cardToSell = storeOrderToBuy.getCard();
+			CardDTO cardToSell = cardWebService.getCard(storeOrderToBuy.getCardId());
 
-			userService.changeMoney(sellerUser, storeOrderToBuy.getPrice());
+			userWebService.changeMoney(sellerUser, storeOrderToBuy.getPrice());
 
 			// Création des transactions
-			StoreTransaction sellerStoreTransaction = createStoreTransaction(sellerUser,storeOrder.getCard(),Action.SELL);
-			StoreTransaction buyerStoreTransaction = createStoreTransaction(storeOrder.getUser(), cardToSell, Action.BUY);
+			StoreTransaction sellerStoreTransaction = createStoreTransaction(sellerUser.getId(),storeOrder.getCardId(),Action.SELL);
+			StoreTransaction buyerStoreTransaction = createStoreTransaction(storeOrder.getUserId(), cardToSell.getId(), Action.BUY);
 
-			cardToSell.setUser(buyerUser);
+			cardToSell.setUserId(buyerUser.getId());
 			//cardService.addCard(cardToSell);
 
 			storeTransactionRepository.save(sellerStoreTransaction);
@@ -66,7 +77,6 @@ public class StoreService {
 
 			storeOrderRepository.delete(storeOrderToBuy);
 		}
-		return false;*/
 		return false;
 		
 	}
